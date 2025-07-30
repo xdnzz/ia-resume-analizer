7f59f4a33ef699d9a5800423d8d0d428fc937d08
Dan Brando <danilosdinizz@gmail.com>
2025-07-30 10:59:52 -0300

    Adding upload route

--- app/routes.ts
+++ app/routes.ts
@@ -2,6 +2,7 @@ import { type RouteConfig, index, route} from "@react-router/dev/routes";
 
 export default [
     index("routes/home.tsx"),
-    route('/auth', 'routes/auth.tsx')
+    route('/auth', 'routes/auth.tsx'),
+    route('/upload', 'routes/upload.tsx')
 
 ] satisfies RouteConfig;
